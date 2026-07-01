55a702718ad7b3f49741c7a1b1636527a4303e57
LinHtutWin0608 <25038212@myrp.edu.sg>
2026-07-01 23:56:15 +0800

    revert DB config to inline credentials; remove .env files

--- database.js
+++ database.js
@@ -1,12 +1,10 @@
-require("dotenv").config();
-
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    user: "root",
+    password: "RP738964$",
+    database: "resource_centre_db",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
